3e545f75ba2ea6a2a7417611d3245d8be7827707
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-12 05:19:42 -0400

    0.0.6

--- roundabout.js
+++ roundabout.js
@@ -1,5 +1,10 @@
 export async function roundabout(options, infractions) {
     const { RoundaboutManager } = await import('./core/RoundaboutManager.js');
     const manager = new RoundaboutManager(options, infractions);
-    return await manager.initialize();
+    const returnObj = await manager.initialize();
+    const initVals = options.initialPropVals || options.defaultPropVals;
+    if (initVals) {
+        (await import('assign-gingerly/assignGingerly.js')).assignGingerly(returnObj[0], initVals);
+    }
+    return returnObj;
 }
--- roundabout.ts
+++ roundabout.ts
@@ -6,5 +6,10 @@ export async function roundabout<TProps = any, TActions = TProps, ETProps = TPro
 ): Promise<[vm: TProps & TActions & RoundaboutReady, propagator: EventTarget]> {
     const { RoundaboutManager } = await import('./core/RoundaboutManager.js');
     const manager = new RoundaboutManager<TProps, TActions, ETProps>(options, infractions);
-    return await manager.initialize();
+    const returnObj = await manager.initialize();
+    const initVals = options.initialPropVals || options.defaultPropVals;
+    if(initVals){
+        (await import('assign-gingerly/assignGingerly.js')).assignGingerly(returnObj[0], initVals);
+    }
+    return returnObj;
 }
